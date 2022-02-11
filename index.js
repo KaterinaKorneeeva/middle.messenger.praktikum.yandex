0e34bb60caa3579d86636ef8466d59b5817db856
Корнеева Екатерина <ekorneeva@uma.tech>
2022-02-11 15:27:17 +0300

    edit file structures

--- index.js
+++ index.js
@@ -1,2 +1,42 @@
-import './src/main.scss';
-console.log('Hello main.js!')
+document.addEventListener('DOMContentLoaded', () => {
+
+    const modalAddPhoto = document.getElementById('modalAddPhoto');
+    const modalEditPass = document.getElementById('modalEditPass');
+    const modalEditProfile = document.getElementById('modalEditProfile');
+
+    const editProfileBtn = document.querySelector(".profile-info__edit-btn");
+    const editPassBtn = document.querySelector(".profile-info__edit-pass");
+    const addProfileBtn = document.querySelector(".profile-info__add-image");
+
+
+    if (addProfileBtn) {
+        addProfileBtn.addEventListener('click', function () {
+            modalAddPhoto.classList.add('active');
+        });
+    }
+
+    if (editPassBtn) {
+        editPassBtn.addEventListener('click', function () {
+            modalEditPass.classList.add('active');
+        });
+    }
+
+    if (editProfileBtn) {
+        editProfileBtn.addEventListener('click', function () {
+            modalEditProfile.classList.add('active');
+        });
+    }
+
+    window.onclick = function (event) {
+        if (event.target == modalAddPhoto) {
+            modalAddPhoto.classList.remove('active');
+        }
+        if (event.target == modalEditPass) {
+            modalEditPass.classList.remove('active');
+        }
+
+        if (event.target == modalEditProfile) {
+            modalEditProfile.classList.remove('active');
+        }
+    }
+})

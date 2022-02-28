1030fe2e2c24b3a7c33ccdf1eb4deec661b80e7b
Корнеева Екатерина <ekorneeva@uma.tech>
2022-02-28 14:16:11 +0300

    test

--- src/pages/Login/login.ts
+++ src/pages/Login/login.ts
@@ -11,27 +11,25 @@ import '../../sass/main.scss'
 export class LoginPage extends Block {
   constructor() {
         super({
-            label: 'Test',
+            label: 'Вход',
             button:  new Button({
-              label: 'Click Test',
+              label: 'Авторизоваться',
               events : {
                 click: () => console.log('clicked')
               }
             })
-})
+        })
   }
 
-  protected initChildren() {
-    console.log('11111', this.props.button);
-    this.children.button = this.props.button;
-  }
+  // protected initChildren() {
+  //   console.log('11111', this.props.button);
+  //   this.children.button = this.props.button; 
+  // }
 
 
   render() {
-    console.log('11111', this.props.button);
-    console.log('this.children.button111', this.children.button );
-    // return this.compile(template);
-    // return this.compile(template, { ...this.props });
-    return this.compile(template, this.children.button )
+    return this.compile(template, { ...this.props });
+    // return this.compile(template, this.children.button )
   }
 }
+

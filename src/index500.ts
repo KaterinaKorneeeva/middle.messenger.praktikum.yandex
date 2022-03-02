94e6e3268e7ca78d52fa7cd4ddd570f705213ebc
Корнеева Екатерина <ekorneeva@uma.tech>
2022-03-02 17:36:12 +0300

    profile

--- src/index500.ts
+++
@@ -1,9 +0,0 @@
-import Error500 from "./pages/Errors/500";
-import  {renderDOM}  from "./utils/renderDOM";
-import './sass/main.scss'
-
-document.addEventListener('DOMContentLoaded', () => {
-  const errorPage = new Error500();
-  
-  renderDOM("#app", errorPage);
-})  
